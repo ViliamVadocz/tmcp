3166775419ddbd49bec3e2a4fb6565decfa48777
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-03-21 13:45:21 +0100

    Make try_convert_direction more strict

--- tmcp/convert.py
+++ tmcp/convert.py
@@ -2,7 +2,11 @@ from typing import List, Any
 
 
 def try_convert_direction(direction: Any) -> List[float]:
-    """Attempts to convert any type into a list of 3 floats."""
+    """
+    Attempts to convert any type into a list of 3 floats.
+    Will raise a TypeError if the type is not something it can convert.
+    It expects two or three coordinates (it will take z to be 0.0 if not specified).
+    """
     # Convert from objects that store their coordinates in a data attribute.
     if hasattr(direction, "data"):
         direction = direction.data
@@ -12,30 +16,19 @@ def try_convert_direction(direction: Any) -> List[float]:
         direction = list(direction)
 
     # Convert from objects with have x, y, z attributes.
-    elif hasattr(direction, "x"):
-        new_dir = [direction.x]
-        if hasattr(direction, "y"):
-            new_dir.append(direction.y)
-            if hasattr(direction, "z"):
-                new_dir.append(direction.z)
+    elif hasattr(direction, "x") and hasattr(direction, "y"):
+        new_dir = [direction.x, direction.y]
+        if hasattr(direction, "z"):
+            new_dir.append(direction.z)
         direction = new_dir
 
     if isinstance(direction, list):
-        l = len(direction)
-        # Pad with zeroes.
-        if l < 3:
-            direction.extend([0.0] * (3 - l))
-        # Take the first three numbers.
-        elif l > 3:
-            direction = direction[:3]
-
-        # Try converting all to floats.
-        for index, axis in enumerate(direction):
-            try:
+        if len(direction) == 2:
+            direction.append(0.0)
+        if len(direction) == 3:
+            # Convert all to floats.
+            for index, axis in enumerate(direction):
                 direction[index] = float(axis)
-            except:
-                direction[index] = 0.0
-    else:
-        direction = [0.0, 0.0, 0.0]
+            return direction
 
-    return direction
+    raise TypeError("Could not convert direction")
